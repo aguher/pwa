bec97d2f86ea4ab33845d91cfee918fa77e4267e
Agustin Herrera Garcia <aherrera@vitio.es>
2023-07-27 11:01:37 +0200

    update base path for docs app

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,9 +1,11 @@
+const { DOCS_URL } = process.env;
+
 const withPWA = require("@imbios/next-pwa")({
   dest: "public",
 });
 
 module.exports = withPWA({
-  basePath: "/docs",
+  basePath: `${DOCS_URL}/docs`,
   reactStrictMode: true,
   transpilePackages: ["ui"],
 });
